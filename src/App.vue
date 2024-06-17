e66ee61f17384290a09f4c57b67b6810481c100d
Annika-Nyholm <146995394+Annika-Nyholm@users.noreply.github.com>
2024-06-17 20:35:17 +0200

    ✨ add app.vue game

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,94 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+	<section id="app">
+		<Home v-if="!gameStarted" @start-game="startGame" />
+		<GameBoard
+			v-else
+			:playerX="playerX"
+			:playerO="playerO"
+      :gameStarted="gameStarted"
+			@update-score="updateScore"
+      @toggle-score-popup="toggleScorePopup"
+      @new-game="newGame"
+      @reset-game="resetGame"
+      @game-won="setWinner"
+			ref="gameboard"
+		/>
+
+		<ScorePopup
+			v-if="showScorePopup"
+			:scores="scores"
+			@close="toggleScorePopup"
+		/>
+		<WinPopup v-if="winner" :winner="winner" @close="resetGame" />
+	</section>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import Home from './components/Home.vue';
+import GameBoard from './components/GameBoard.vue';
+import ScorePopup from './components/ScorePopup.vue';
+import WinPopup from './components/WinPopup.vue';
+
+const playerX = ref(localStorage.getItem('playerX') || '');
+const playerO = ref(localStorage.getItem('playerO') || '');
+const gameStarted = ref(playerX.value && playerO.value ? true : false);
+const showScorePopup = ref(false);
+const scores = ref(JSON.parse(localStorage.getItem('scores') || '{}'));
+const winner = ref<string | null>(null);
+const gameboard = ref<InstanceType<typeof GameBoard> | null>(null);
+
+onMounted(() => {
+	const savedPlayerX = localStorage.getItem('playerX');
+	const savedPlayerO = localStorage.getItem('playerO');
+	const savedBoard = localStorage.getItem('board');
+
+	if (savedPlayerX && savedPlayerO && savedBoard) {
+		playerX.value = savedPlayerX;
+		playerO.value = savedPlayerO;
+		gameStarted.value = true;
+	}
+});
+
+const startGame = (players: { playerX: string, playerO: string }) => {
+  playerX.value = players.playerX;
+  playerO.value = players.playerO;
+	localStorage.setItem('playerX', playerX.value);
+	localStorage.setItem('playerO', playerO.value);
+	gameStarted.value = true;
+};
+
+const updateScore = (winner: string) => {
+	if (!scores.value[winner]) {
+		scores.value[winner] = 0;
+	}
+	scores.value[winner]++;
+	localStorage.setItem('scores', JSON.stringify(scores.value));
+};
+
+const setWinner = (winnerValue: string) => {
+  winner.value = winnerValue === 'X' ? playerX.value : winnerValue === 'O' ? playerO.value : 'Draw';
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
+const toggleScorePopup = () => {
+	showScorePopup.value = !showScorePopup.value;
+};
+
+const resetGame = () => {
+	gameboard.value?.resetBoard();
+  winner.value = null;
+};
+
+const newGame = () => {
+	localStorage.clear();
+	gameStarted.value = false;
+	playerX.value = '';
+	playerO.value = '';
+	scores.value = {};
+  winner.value = null;
+};
+</script>
+
+<style lang="scss" scoped>
+
 </style>
