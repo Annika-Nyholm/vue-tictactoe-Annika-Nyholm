c4efc9686875976509410a1c7a7c16cdd7561dc9
Annika-Nyholm <146995394+Annika-Nyholm@users.noreply.github.com>
2024-06-17 20:46:29 +0200

    ✨ add popups, win and scores

---
+++ src/components/ScorePopup.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="popup">
+      <div class="popup-content">
+        <Score :scores="scores" />
+        <button @click="close">Close</button>
+      </div>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  import Score from './Score.vue';
+  
+  const props = defineProps<{
+    scores: { [key: string]: number };
+  }>();
+  
+  const emit = defineEmits<{
+    (e: 'close'): void;
+  }>();
+
+  const close = () => {
+    emit('close');
+  }
+  </script>
+  
+  <style lang="scss" scoped>
+  </style>
---
+++ src/components/WinPopup.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="popup">
+		<div class="popup-content">
+			<h2>{{ winner === 'Draw' ? 'Draw' : `${winner} wins!` }}</h2>
+			<button @click="close">Close</button>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+	winner: string;
+}>();
+
+const emit = defineEmits<{
+	(e: 'close'): void;
+}>();
+
+const close = () => {
+	emit('close');
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
