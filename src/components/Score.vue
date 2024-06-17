992369e30a6af6afad29074b7edad5795b97fa86
Annika-Nyholm <146995394+Annika-Nyholm@users.noreply.github.com>
2024-06-17 20:43:55 +0200

    ✨ add score

---
+++ src/components/Score.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="score">
+		<h2>Scores</h2>
+		<p>Player X: {{ scores.X || 0 }}</p>
+		<p>Player O: {{ scores.O || 0 }}</p>
+	</div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+	scores: { [key: string]: number };
+}>();
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/styles/variables';
+.score {
+	text-align: center;
+	margin-top: 20px;
+
+}
+</style>
